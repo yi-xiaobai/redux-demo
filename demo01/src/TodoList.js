1d4fd2467b295fc100e485b7093df81cf1868738
yyll <lyyl6270@163.com>
2023-09-07 21:09:43 +0800

    feat:redux

--- demo01/src/TodoList.js
+++ demo01/src/TodoList.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react'
 import { Input, Button, List } from 'antd'
-
-const data = [
-    '早8点开晨会，分配今天的开发工作',
-    '早9点和项目经理作开发需求讨论会',
-    '晚5:30对今日代码进行review'
-]
+import store from './store'
 
 class ToDoList extends Component {
     constructor(props) {
         super(props);
+
+        // 获取store仓库值
+        this.state = store.getState()
+
+        // 监听store值  如果state变化了 自动执行processState函数
+        store.subscribe(this.processState)
     }
     state = {}
     render() {
         return (
             <div style={{ margin: '10px auto', width: '500px' }}>
                 <div>
-                    <Input placeholder='请输入' style={{ width: '250px', height: '30px' }} />
+                    <Input placeholder={this.state.value} style={{ width: '250px', height: '30px' }}
+                        onChange={this.handleChangeValue} />
                     <Button type='primary'>增加</Button>
                 </div>
 
-                <div style={{marginTop:'10px',width:'300px'}}>
+                <div style={{ marginTop: '10px', width: '300px' }}>
                     <List bordered
-                        dataSource={data}
+                        dataSource={this.state.list}
                         renderItem={item => <List.Item>{item}</List.Item>}></List>
                 </div>
             </div>
         );
+    }
+
+
+    handleChangeValue = (e) => {
+        console.log('==>Get value', e.target.value);
+        // 定义action
+        let action = {
+            type: 'changeInput',
+            value: e.target.value
+        }
+        // 发出action  store自动调用reducer进行计算
+        store.dispatch(action)
+    }
+
+
+    processState = () => {
+        console.log('==>Get 监听state');
+        // 获取仓库state值
+        console.log('==>Get state', store.getState());
+        this.setState(store.getState())
     }
 }
 
--- demo01/src/store/reducer.js
+++ demo01/src/store/reducer.js
@@ -1,5 +1,23 @@
-const defaultState = {}
+const defaultState = {
+    value: "write something",
+    list: [
+        '早8点开晨会，分配今天的开发工作',
+        '早9点和项目经理作开发需求讨论会',
+        '晚5:30对今日代码进行review'
+    ]
+}
 
-export default (state = defaultState, action) => {
+const Reducer = (state = defaultState, action) => {
+    console.log('==>Get state,,action', state, action);
+
+    // 收到了用户提交过来的dispatch  进行处理state
+    if (action.type === 'changeInput') {
+        return {
+            ...state,
+            ...{ value: action.value }
+        }
+    }
     return state
 }
+
+export default Reducer
